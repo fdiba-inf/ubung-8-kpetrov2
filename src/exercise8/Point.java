023d0ed1b4a9d68741b0949002a6243f347926b0
kpetrov2 <konstantin.petrov@fdiba.tu-sofia.bg>
2020-11-22 21:44:21 +0000

    22.11

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,40 +1,50 @@
 package exercise8;
 
 public class Point {
-    // Define attributes
+    private double x;
+    private double y;
 
     public Point() {
-        // Initialize attributes to 0
+        x = 0;
+        y = 0;
     }
 
     public Point(double x, double y) {
-        // Initialize attributes by coping x and y parameters
+        this.x = x;
+        this.y = y;
     }
 
     public Point(Point otherPoint) {
-        // Initialize attributes by coping attributes of otherPoint
+        x = otherPoint.x;
+        y = otherPoint.y;
     }
 
     public void initialize() {
-        // Change attributes through console
+        System.out.print("Enter the value of x: ");
+        x = Utils.INPUT.nextDouble();
+        System.out.print("Enter the value of y: ");
+        y = Utils.INPUT.nextDouble();
     }
 
     public void translate(double xDelta, double yDelta) {
-        // Change attributes by adding xDelta and yDelta
+        x += xDelta;
+        y += yDelta;
     }
 
     public Point createNewTranslatedPoint(double xDelta, double yDelta) {
-        // Create new point which is translated relative to this point
-        return null;
+        double newX = x + xDelta;
+        double newY = y + yDelta;
+        return new Point(newX, newY);
     }
 
     public boolean equals(Point otherPoint) {
-        // Compare attributes of this point to attributes of otherPoint for equality
-        return false;
+        boolean checkx = Utils.equals(x, otherPoint.x);
+        boolean checky = Utils.equals(y, otherPoint.y);
+        return checkx && checky;
     }
 
     public String toString() {
         // Represent attributes as string
-        return null;
+        return "(" + x + ", " + y + ")";
     }
 }
